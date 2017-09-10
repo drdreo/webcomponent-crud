d4420f79863b4f1df3ca1589ea89412ab9d1fe09
drdreo <drdreo95@gmail.com>
2017-09-10 15:00:51 +0200

    added login component

--- server/components/models/users.js
+++ server/components/models/users.js
@@ -1,11 +1,14 @@
 //Require mongoose package
 // const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
         username: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         },
         password: {
             type: String,
@@ -28,6 +31,34 @@ const userSchema = new Schema({
 
 //set collection here or in the model creation or in the schema creation
 userSchema.set("collection", "users");
+
+
+userSchema.pre('save', function (next) {
+    let user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        cb(err, isMatch);
+    });
+};
 
 //first argument is a useless model name, 3rd is the actual collection name
 const UsersModel = module.exports = mongoose.model('UserSchema', userSchema, "users");
@@ -53,6 +84,38 @@ module.exports.deleteUserById = (id, callback) => {
     let query = {_id: id};
     UsersModel.remove(query, callback);
 };
+
+//Here we need to pass an id parameter to Model.remove
+module.exports.getUserByUsername = (username, password, callback) => {
+    let query = {
+        username: {$regex: username}
+    };
+    UsersModel.findOne(query, function (err, result) {
+        // test a matching password
+
+        callback(err, result);
+    });
+    console.log("done getting username");
+};
+
+// fetch user and test password verification
+module.exports.validateUser = (username, password, callback) => {
+
+    UsersModel.findOne({username: username}, function (err, user) {
+        // if user exists
+        if (user) {
+            user.comparePassword(password, function (err, isMatch) {
+
+                console.log('Passwords  match:', isMatch);
+                callback(err, isMatch);
+            });
+        } else {
+            callback(err, false);
+        }
+
+    });
+};
+
 
 // example schema types
 // var schema = new Schema({
